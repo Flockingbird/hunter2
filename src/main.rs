b9156ceabc3df281e49b5f92da28920407f690da
Bèr Kessels <ber@berk.es>
2021-03-17 15:49:35 +0100

    Add commandline options to follow or view past items

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,92 @@
-use elefren::prelude::*;
+use elefren::entities::account::Account;
 use elefren::entities::event::Event;
 use elefren::entities::status::Tag;
-use elefren::entities::account::Account;
+use elefren::prelude::*;
 
 use elefren::helpers::cli;
 use elefren::helpers::env;
 
+use getopts::Options;
+
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mastodon = if let Ok(data) = env::from_env() {
-      Mastodon::from(data)
-    } else {
-        register()?
+    let args: Vec<String> = std::env::args().collect();
+    let program = args[0].clone();
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "print this help menu");
+    opts.optflag("r", "register", "register hunter2 with your instance.");
+    opts.optflag("f", "follow", "follow live updates.");
+    opts.optflag("p", "past", "fetch past updates.");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            panic!(f.to_string())
+        }
     };
+
+    // print help when requested
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return Ok(());
+    }
+
+    if matches.opt_present("r") {
+        register()?;
+        return Ok(());
+    }
+
+    let data = env::from_env().unwrap();
+    let mastodon = Mastodon::from(data);
 
     let you = mastodon.verify_credentials()?;
 
     welcome_msg(you);
 
-    for event in mastodon.streaming_public()? {
-        match event {
-            Event::Update(ref status) => {
+    if matches.opt_present("p") {
+        // TODO: This method will return duplicates. So we should deduplicate
+        for tag in job_tags() {
+            for status in mastodon.get_tagged_timeline(tag, false)? {
                 if has_job_related_tags(&status.tags) {
                     println!("{:#?}", status);
                 }
-            },
-            Event::Notification(ref _notification) => { /* .. */ },
-            Event::Delete(ref _id) => { /* .. */ },
-            Event::FiltersChanged => { /* .. */ },
+            }
         }
     }
+
+    if matches.opt_present("f") {
+        for event in mastodon.streaming_public()? {
+            match event {
+                Event::Update(ref status) => {
+                    if has_job_related_tags(&status.tags) {
+                        println!("{:#?}", status);
+                    }
+                }
+                Event::Notification(ref _notification) => { /* .. */ }
+                Event::Delete(ref _id) => { /* .. */ }
+                Event::FiltersChanged => { /* .. */ }
+            }
+        }
+    }
+
     Ok(())
 }
 
 fn register() -> Result<Mastodon, Box<dyn Error>> {
     let registration = Registration::new(std::env::var("BASE").expect("BASE"))
-                                    .client_name("hunter2")
-                                    .build()?;
+        .client_name("hunter2")
+        .build()?;
     let mastodon = cli::authenticate(registration)?;
 
     // Print the ENV var to screen for copying into whatever we use (.env)
     println!("Save these env vars in e.g. .env\n");
     println!("export {}=\"{}\"", "BASE", &mastodon.data.base);
     println!("export {}=\"{}\"", "CLIENT_ID", &mastodon.data.client_id);
-    println!("export {}=\"{}\"", "CLIENT_SECRET", &mastodon.data.client_secret);
+    println!(
+        "export {}=\"{}\"",
+        "CLIENT_SECRET", &mastodon.data.client_secret
+    );
     println!("export {}=\"{}\"", "REDIRECT", &mastodon.data.redirect);
     println!("export {}=\"{}\"\n", "TOKEN", &mastodon.data.token);
 
@@ -55,13 +97,29 @@ fn welcome_msg(you: Account) {
     println!("We've sent out {} to hunt for jobs...", you.display_name);
 }
 
-fn has_job_related_tags(tags: &Vec<Tag>) -> bool {
-    let job_tags = vec!["jobs", "jobsearch", "joboffer", "hiring", "vacancy"];
+fn job_tags() -> Vec<String> {
+    vec![
+        "jobs".to_string(),
+        "jobsearch".to_string(),
+        "joboffer".to_string(),
+        "hiring".to_string(),
+        "vacancy".to_string(),
+    ]
+}
 
+fn has_job_related_tags(tags: &Vec<Tag>) -> bool {
     // INK: debugging why checking for these tags does not work.
     // Probably best check first for a single tag?
-    !tags.is_empty() &&
-      tags.iter().map(|t| t.name.as_str() ).any(|e| job_tags.contains(&e))
+    !tags.is_empty()
+        && tags
+            .iter()
+            .map(|t| t.name.to_owned())
+            .any(|e| job_tags().contains(&e))
+}
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} TEMPLATE_FILE [options]", program);
+    print!("{}", opts.usage(&brief));
 }
 
 #[cfg(test)]
@@ -70,15 +128,24 @@ mod tests {
 
     #[test]
     fn test_has_job_related_tags_with_jobs_tag() {
-        let tags = vec![Tag { url: "".to_string(), name: "jobs".to_string() }];
+        let tags = vec![Tag {
+            url: "".to_string(),
+            name: "jobs".to_string(),
+        }];
         assert!(has_job_related_tags(&tags))
     }
 
     #[test]
     fn test_has_job_related_tags_with_multiple_tags() {
         let tags = vec![
-            Tag { url: "".to_string(), name: "jobs".to_string() },
-            Tag { url: "".to_string(), name: "steve".to_string() },
+            Tag {
+                url: "".to_string(),
+                name: "jobs".to_string(),
+            },
+            Tag {
+                url: "".to_string(),
+                name: "steve".to_string(),
+            },
         ];
         assert!(has_job_related_tags(&tags))
     }
@@ -91,7 +158,10 @@ mod tests {
 
     #[test]
     fn test_has_no_job_related_tags_without_allowed_tags() {
-        let tags = vec![Tag { url: "".to_string(), name: "steve".to_string() }];
+        let tags = vec![Tag {
+            url: "".to_string(),
+            name: "steve".to_string(),
+        }];
         assert!(!has_job_related_tags(&tags))
     }
 }
