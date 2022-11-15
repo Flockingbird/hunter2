8c77db5f1dd946554fcb4108be1a3ae7ff31db7b
Bèr Kessels <ber@berk.es>
2022-11-15 15:46:12 +0100

    Rename function to determine robots noindex tag.

--- bot/src/hunter2/may_index.rs
+++ bot/src/hunter2/may_index.rs
@@ -4,7 +4,7 @@ use std::io::Read;
 
 pub fn may_index(url: &str) -> bool {
     let html = fetch(url);
-    determine_meta_robots(&html)
+    meta_robots_is_allowed(&html)
 }
 
 fn fetch(url: &str) -> String {
@@ -17,7 +17,7 @@ fn fetch(url: &str) -> String {
     body
 }
 
-fn determine_meta_robots(html: &str) -> bool {
+fn meta_robots_is_allowed(html: &str) -> bool {
     let fragment = Html::parse_fragment(html);
     let selector = Selector::parse(r#"meta[name="robots"]"#).unwrap();
 
@@ -39,39 +39,39 @@ mod tests {
     #[test]
     fn determine_meta_robots_has_no_meta_tag() {
         let html = "<head><title>Title</title></head>".to_string();
-        assert!(determine_meta_robots(&html));
+        assert!(meta_robots_is_allowed(&html));
     }
 
     #[test]
     fn determine_meta_robots_has_meta_tag_no_noindex() {
         let html = "<head><title>Title</title><meta name=\"robots\" content=\"index\"/></head>"
             .to_string();
-        assert!(determine_meta_robots(&html));
+        assert!(meta_robots_is_allowed(&html));
     }
 
     #[test]
     fn determine_meta_robots_has_meta_tag_no_content() {
         let html = "<head><title>Title</title><meta name=\"robots\"/></head>".to_string();
-        assert!(determine_meta_robots(&html));
+        assert!(meta_robots_is_allowed(&html));
     }
 
     #[test]
     fn determine_meta_robots_has_meta_tag_none() {
         let html =
             "<head><title>Title</title><meta name=\"robots\" content=\"none\"/></head>".to_string();
-        assert!(!determine_meta_robots(&html));
+        assert!(!meta_robots_is_allowed(&html));
     }
 
     #[test]
     fn determine_meta_robots_has_meta_tag_noindex() {
         let html = "<head><title>Title</title><meta name=\"robots\" content=\"noindex\"/></head>"
             .to_string();
-        assert!(!determine_meta_robots(&html));
+        assert!(!meta_robots_is_allowed(&html));
     }
 
     #[test]
     fn determine_meta_robots_has_meta_tag_several() {
         let html = "<head><title>Title</title><meta name=\"robots\" content=\"nosnippet noindex nofollow\"/></head>".to_string();
-        assert!(!determine_meta_robots(&html));
+        assert!(!meta_robots_is_allowed(&html));
     }
 }
